9744c6d0fc45cf5b1b6d11efcbbe784712415a8d
sakshi karn <sakshikarna@gmail.com>
2025-11-09 14:04:05 +0545

    changes

--- app.js
+++ app.js
@@ -4,17 +4,29 @@
  * <div id="parents" 
  *   <div id="child">
  *      <h1>Hello World</h1>
+ *         <h2>This is h2 tag</h2>
+ *   </div>
+ * <div id="child">
+ *      <h1>Hello World</h1>
+ *         <h2>This is h2 tag</h2>
  *   </div>
  * </div>
- * 
+ * ReactElement(object) =>  HTML(Browser Understands)
  * 
  */
 const parents = React.createElement(
     "div",{id : "parents"},
-    React.createElement("div",{id : "child"},
-        React.createElement("h1",{},"I  am a h1 tag inside child div")
-    )
-);
+    [React.createElement("div",{id : "child"},
+       [
+        React.createElement("h1",{},"I  am a h1 tag inside child div"),
+        React.createElement("h2",{},"I  am a h2 tag inside child div")
+    ]),
+    React.createElement("div",{id : "child2"},
+       [
+        React.createElement("h1",{},"I  am a h1 tag inside child div"),
+        React.createElement("h2",{},"I  am a h2 tag inside child div")
+    ])
+]);
 
 /**const heading = React.createElement(
     "h1",
