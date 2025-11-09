697048697baca5c6e49df2546b9306c98e3309d2
sakshi karn <sakshikarna@gmail.com>
2025-11-09 13:20:28 +0545

    changes done

---
+++ app.js
@@ -0,0 +1,8 @@
+const heading = React.createElement(
+    "h1",
+    {id : "heading" , xyz : "abc"},
+    "Hello World form React Inside it");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
