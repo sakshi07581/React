503a2c5e9989569e4b2ff4c21e126a687d8baa58
sakshi karn <sakshikarna@gmail.com>
2025-11-09 13:50:45 +0545

    changes done

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
-const heading = React.createElement(
+/**
+ * 
+ * 
+ * <div id="parents" 
+ *   <div id="child">
+ *      <h1>Hello World</h1>
+ *   </div>
+ * </div>
+ * 
+ * 
+ */
+const parents = React.createElement(
+    "div",{id : "parents"},
+    React.createElement("div",{id : "child"},
+        React.createElement("h1",{},"I  am a h1 tag inside child div")
+    )
+);
+
+/**const heading = React.createElement(
     "h1",
     {id : "heading" , xyz : "abc"},
-    "Hello World form React Inside it"
-);
-console.log(heading);
+    "Hello World form React Inside it");*/
+
+console.log(parents); // This will print an object
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading); 
+root.render(parents); 
